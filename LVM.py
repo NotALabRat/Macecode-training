b7b6edb6fe6e309f67fbcc18a5c1f2098d8610cf
NotALabRat <163991615+NotALabRat@users.noreply.github.com>
2025-07-12 11:55:48 +0530

    Update LVM.py

--- LVM.py
+++ LVM.py
@@ -5,15 +5,15 @@ for i in range(n):
     instructions.append(input().split())
 
 stack=[]
-register = 0
-i = 0
+register=0
+i=0
 
 while i<len(instructions):
     instr =instructions[i]
 
     if instr[0]=='PUSH':
         stack.insert(0,int(instr[1]))  
-        i+= 1
+        i+=1
 
     elif instr[0]=='STORE':
         register=stack.pop(0)  
@@ -21,19 +21,19 @@ while i<len(instructions):
 
     elif instr[0]=='LOAD':
         stack.insert(0,register)  
-        i+= 1
+        i+=1
 
     elif instr[0]=='PLUS':
         a=stack.pop(0)
         b=stack.pop(0)
         stack.insert(0,a+b)
-        i+= 1
+        i+=1
 
     elif instr[0]=='TIMES':
         a=stack.pop(0)
         b=stack.pop(0)
         stack.insert(0,a*b)
-        i+= 1
+        i+=1
 
     elif instr[0]=='IFZERO':
         value=stack.pop(0)
